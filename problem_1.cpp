659f6fc7f04afea7d89062c91c1adafd5fdcdd3c
akbarjuramirza <118677024+akbarjuramirza@users.noreply.github.com>
2022-11-23 16:39:44 +0500

    Create problem_1.cpp

---
+++ problem_1.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    
+    /*Write a program to print the address of a variable whose value is input from user.*/
+    //Problem-1
+    
+    int number;
+    cout << "enter the number: ";
+    cin >> number;
+    
+    int *n_ptr = nullptr;
+    n_ptr = &number;
+    cout << n_ptr << endl;
+       
+    return 0;
+}
