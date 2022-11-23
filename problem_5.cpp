6ff0de1781e7d569873b09df3cff2976782583e0
akbarjuramirza <118677024+akbarjuramirza@users.noreply.github.com>
2022-11-23 16:44:36 +0500

    Create problem_5.cpp

---
+++ problem_5.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    /*Write a program to print a number which is entered from keyboard using pointer.*/
+    //Problem-5
+    
+    int *x_ptr = nullptr;
+    cout << "enter the number: ";
+    cin >> *x_ptr;
+    cout << *x_ptr << endl;
+    
+    
+    return 0;
+}
